1bde85a50a1d5fb1c2de4e5887fb22d54d269b18
Hastho_Hastho <hastho.rahtomo@phincon.com>
2025-03-14 04:06:32 +0700

    Initial commit

---
+++ lib/src/main/java/testBelajar.java
@@ -0,0 +1,5 @@
+public class testBelajar {
+    public static void main(String[] args){
+        System.out.println("Halo halo");
+    }
+}
---
+++ lib/src/test/java/testGradleBelajar.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class testGradleBelajar {
+    @Test
+    public void simpleTest() {
+    assert 2 == (1+1);
+    } 
+}
